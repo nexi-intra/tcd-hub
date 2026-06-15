04dc1bfa1e4d4c9b9ffa60ef37f6b2c6fff5b924
Jacob Remmer <jacob.remmer@nexigroup.com>
2026-06-15 07:46:36 +0000

    Generated by Spark: Add even more spacing if the hub needs to be positioned lower

--- src/views/Hub.tsx
+++ src/views/Hub.tsx
@@ -242,7 +242,7 @@ export function Hub({ onNavigate, onLogout, userEmail }: HubProps) {
         }}
         userEmail={userEmail}
       />
-      <div className="container mx-auto px-4 sm:px-6 pt-72 sm:pt-80 pb-12 sm:pb-20 max-w-7xl relative z-10">
+      <div className="container mx-auto px-4 sm:px-6 pt-96 sm:pt-[26rem] pb-12 sm:pb-20 max-w-7xl relative z-10">
         <motion.header 
           className="text-center mb-16 sm:mb-20"
           initial={{ opacity: 0, y: -20 }}
